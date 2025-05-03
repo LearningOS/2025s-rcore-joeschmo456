83dc5c4fcda1ec4ea6d5d4aefe809101fda580e2
joeschmo456 <imzhangxiangbo@gmail.com>
2025-05-03 09:43:30 +0800

    ch3

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -14,7 +14,7 @@ mod switch;
 #[allow(clippy::module_inception)]
 mod task;
 
-use crate::config::MAX_APP_NUM;
+use crate::config::{MAX_APP_NUM, MAX_SYSCALL_NUM};
 use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
 use lazy_static::*;
@@ -54,6 +54,7 @@ lazy_static! {
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
             task_status: TaskStatus::UnInit,
+            syscall_times: [0; MAX_SYSCALL_NUM],
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
@@ -135,6 +136,21 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+    
+    /// Get how many times one syscall has been called
+    fn get_syscall_times(&self, _syscall_id: usize) -> i32 {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        let cnt = inner.tasks[current].syscall_times[_syscall_id];
+        cnt
+    }
+
+    /// Increase the times one syscall has been called
+    fn inc_syscall_times(&self, _syscall_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].syscall_times[_syscall_id] += 1;
+    }
 }
 
 /// Run the first task in task list.
@@ -168,4 +184,14 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// Get how many times one syscall has been called
+pub fn get_syscall_times(syscall_id: usize) -> i32 {
+    TASK_MANAGER.get_syscall_times(syscall_id)
+}
+
+/// Increase the times one syscall has been called
+pub fn inc_syscall_times(syscall_id: usize) {
+    TASK_MANAGER.inc_syscall_times(syscall_id);
 }
