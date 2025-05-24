dd9131ee8939e55b6362336a7b0fd6cdf0afb8c8
joeschmo456 <imzhangxiangbo@gmail.com>
2025-05-24 10:41:58 +0800

    ch8

--- os/src/sync/mod.rs
+++ os/src/sync/mod.rs
@@ -1,11 +1,13 @@
 //! Synchronization and interior mutability primitives
 
 mod condvar;
+mod deadlock_detection;
 mod mutex;
 mod semaphore;
 mod up;
 
 pub use condvar::Condvar;
+pub use deadlock_detection::Banker;
 pub use mutex::{Mutex, MutexBlocking, MutexSpin};
 pub use semaphore::Semaphore;
 pub use up::UPSafeCell;
